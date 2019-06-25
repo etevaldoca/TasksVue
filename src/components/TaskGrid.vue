5a8d62675f449b688566444d266bc2421ef3e892
etevaldoca <etevaldosilva@yahoo.com>
2019-06-25 08:57:32 -0300

    minhas tarefas com componentes vue

---
+++ src/components/TaskGrid.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="task-grid">
+    <template v-if="tasks.length">
+      <Task v-for="task in tasks" :key="task.name" :task="task"></Task>
+    </template>
+    <p v-else>Sua vida esta em dias :)</p>
+  </div>
+</template>
+
+<script>
+import Task from "./Task";
+export default {
+  components: { Task },
+  props: {
+    tasks: { type: Array, required: true }
+  }
+};
+</script>
+
+<style>
+</style>
